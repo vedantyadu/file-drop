74f58b6d280ec8837751858ef42e605a6c75cc7e
Vedant Tiwari <vedantyadu@gmail.com>
2025-05-15 15:55:47 +0530

    added broadcast address function

--- src/connection/main.py
+++ src/connection/main.py
@@ -1,40 +1,49 @@
 import socket
 import time
 import threading
+import ipaddress
+
+
 
 BROADCAST_INTERVAL = 2
 LISTEN_INTERVAL = BROADCAST_INTERVAL * 2
 
 BROADCAST_PORT = 50000
+SUBNETMASK = "255.255.255.0"
 PRESENCE_MESSAGE = "CONNECTION::HELLO"
 
 available_peers = set()
 
 
-def get_self_ip():
+def get_broadcast_address(self_address: str, subnetmask: str):
+    ip = ipaddress.IPv4Interface(f"{self_address}/{subnetmask}")
+    return str(ip.network.broadcast_address)
+
+
+def get_self_address():
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         soc.connect(('8.8.8.8', 80))
         local_network_ip = soc.getsockname()[0]
-        
         return local_network_ip
     finally:
         soc.close()
 
 
-def broadcast_presence():
+def broadcast_presence(self_address: str):
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    broadcast_address = get_broadcast_address(self_address, SUBNETMASK)
 
     while True:
-        soc.sendto(PRESENCE_MESSAGE.encode(), ('192.168.1.255', BROADCAST_PORT))
+        soc.sendto(PRESENCE_MESSAGE.encode(),
+                   (broadcast_address, BROADCAST_PORT))
         time.sleep(BROADCAST_INTERVAL)
 
 
-def listen_for_peers(self_ip: str):
+def listen_for_peers(self_address: str):
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    soc.bind((self_ip, BROADCAST_PORT))
-    print(soc)
+    soc.bind((self_address, BROADCAST_PORT))
 
     while True:
         data, addr = soc.recvfrom(1024)
@@ -42,21 +51,22 @@ def listen_for_peers(self_ip: str):
         if data.decode() == PRESENCE_MESSAGE:
             ip = addr[0]
 
-            if ip != self_ip:
+            if ip != self_address:
                 available_peers.add(ip)
 
 
 if __name__ == "__main__":
-    self_ip = get_self_ip()
-    print(f"Self IP: {self_ip}")
+    self_address = get_self_address()
+    print(f"Self Address: {self_address}")
 
-    broadcast_thread = threading.Thread(target=broadcast_presence)
+    broadcast_thread = threading.Thread(target=broadcast_presence, args=[self_address])
     broadcast_thread.start()
 
-    listen_thread = threading.Thread(target=listen_for_peers, args=[self_ip])
+    listen_thread = threading.Thread(target=listen_for_peers, args=[self_address])
     listen_thread.start()
 
     while True:
         available_peers.clear()
         time.sleep(LISTEN_INTERVAL)
         print("Available peers:", list(available_peers))
+        
